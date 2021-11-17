a8d39aa93c041d0284e17df8c5a35b03fb16397a
imaamaakbar <imamakbar165@gmail.com>
2021-11-18 02:41:17 +0700

    Update buat Seacrh

--- app/src/main/java/com/keluts/warekfilm/fragment/FragmentMovie.java
+++ app/src/main/java/com/keluts/warekfilm/fragment/FragmentMovie.java
@@ -3,10 +3,12 @@ package com.keluts.warekfilm.fragment;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -41,10 +43,12 @@ public class FragmentMovie extends Fragment implements MovieHorizontalAdapter.on
     private MovieHorizontalAdapter movieHorizontalAdapter;
     private MovieAdapter movieAdapter;
     private ProgressDialog progressDialog;
+    private SearchView searchFilm;
     private List<ModelMovie> moviePlayNow = new ArrayList<>();
     private List<ModelMovie> moviePopular = new ArrayList<>();
 
-
+    public FragmentMovie() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -54,6 +58,30 @@ public class FragmentMovie extends Fragment implements MovieHorizontalAdapter.on
         progressDialog.setTitle("Mohon Tunggu");
         progressDialog.setCancelable(false);
         progressDialog.setMessage("Sedang menampilkan data");
+
+        searchFilm = rootView.findViewById(R.id.searchFilm);
+        searchFilm.setQueryHint(getString(R.string.search_film));
+        searchFilm.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                setSearchMovie(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.equals(""))
+                    getMovie();
+                return false;
+            }
+        });
+
+        int searchPlateId = searchFilm.getContext().getResources()
+                .getIdentifier("android:id/search_plate", null, null);
+        View searchPlate = searchFilm.findViewById(searchPlateId);
+        if (searchPlate != null) {
+            searchPlate.setBackgroundColor(Color.TRANSPARENT);
+        }
 
         rvNowPlaying = rootView.findViewById(R.id.rvNowPlaying);
         rvNowPlaying.setHasFixedSize(true);
@@ -70,9 +98,49 @@ public class FragmentMovie extends Fragment implements MovieHorizontalAdapter.on
         return rootView;
     }
 
+    private void setSearchMovie(String query) {
+        progressDialog.show();
+        AndroidNetworking.get(ApiEndpoint.BASEURL + ApiEndpoint.SEARCH_MOVIE
+                + ApiEndpoint.APIKEY + ApiEndpoint.LANGUAGE + ApiEndpoint.QUERY + query)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            progressDialog.dismiss();
+                            moviePopular = new ArrayList<>();
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                ModelMovie dataApi = new ModelMovie();
+                                SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMMM yyyy");
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+                                String datePost = jsonObject.getString("release_date");
 
+                                dataApi.setId(jsonObject.getInt("id"));
+                                dataApi.setTitle(jsonObject.getString("title"));
+                                dataApi.setVoteAverage(jsonObject.getDouble("vote_average"));
+                                dataApi.setOverview(jsonObject.getString("overview"));
+                                dataApi.setReleaseDate(formatter.format(dateFormat.parse(datePost)));
+                                dataApi.setPosterPath(jsonObject.getString("poster_path"));
+                                dataApi.setBackdropPath(jsonObject.getString("backdrop_path"));
+                                dataApi.setPopularity(jsonObject.getString("popularity"));
+                                moviePopular.add(dataApi);
+                                showMovie();
+                            }
+                        } catch (JSONException | ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
-
+                    @Override
+                    public void onError(ANError anError) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getActivity(), "Tidak ada Signal!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
     private void getMovieHorizontal() {
         progressDialog.show();
--- app/src/main/java/com/keluts/warekfilm/fragment/FragmentTV.java
+++ app/src/main/java/com/keluts/warekfilm/fragment/FragmentTV.java
@@ -3,6 +3,7 @@ package com.keluts.warekfilm.fragment;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
@@ -42,6 +44,7 @@ public class FragmentTV extends Fragment implements TvHorizontalAdapter.onSelect
     private TvHorizontalAdapter tvHorizontalAdapter;
     private TvAdapter tvAdapter;
     private ProgressDialog progressDialog;
+    private SearchView searchFilm;
     private List<ModelTV> tvPlayNow = new ArrayList<>();
     private List<ModelTV> tvPopular = new ArrayList<>();
 
@@ -56,9 +59,29 @@ public class FragmentTV extends Fragment implements TvHorizontalAdapter.onSelect
         progressDialog.setCancelable(false);
         progressDialog.setMessage("Sedang menampilkan data");
 
+        searchFilm = rootView.findViewById(R.id.searchFilm);
+        searchFilm.setQueryHint(getString(R.string.search_film));
+        searchFilm.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                setSearchTv(query);
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.equals(""))
+                    getFilmTV();
+                return false;
+            }
+        });
 
-
+        int searchPlateId = searchFilm.getContext().getResources()
+                .getIdentifier("android:id/search_plate", null, null);
+        View searchPlate = searchFilm.findViewById(searchPlateId);
+        if (searchPlate != null) {
+            searchPlate.setBackgroundColor(Color.TRANSPARENT);
+        }
 
         rvNowPlaying = rootView.findViewById(R.id.rvNowPlaying);
         rvNowPlaying.setHasFixedSize(true);
@@ -75,7 +98,50 @@ public class FragmentTV extends Fragment implements TvHorizontalAdapter.onSelect
         return rootView;
     }
 
+    private void setSearchTv(String query) {
+        progressDialog.show();
+        AndroidNetworking.get(ApiEndpoint.BASEURL + ApiEndpoint.SEARCH_TV
+                + ApiEndpoint.APIKEY + ApiEndpoint.LANGUAGE + ApiEndpoint.QUERY + query)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            progressDialog.dismiss();
+                            tvPopular = new ArrayList<>();
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                ModelTV dataApi = new ModelTV();
+                                SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMMM yyyy");
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+                                String datePost = jsonObject.getString("first_air_date");
 
+                                dataApi.setId(jsonObject.getInt("id"));
+                                dataApi.setName(jsonObject.getString("name"));
+                                dataApi.setVoteAverage(jsonObject.getDouble("vote_average"));
+                                dataApi.setOverview(jsonObject.getString("overview"));
+                                dataApi.setReleaseDate(formatter.format(dateFormat.parse(datePost)));
+                                dataApi.setPosterPath(jsonObject.getString("poster_path"));
+                                dataApi.setBackdropPath(jsonObject.getString("backdrop_path"));
+                                dataApi.setPopularity(jsonObject.getString("popularity"));
+                                tvPopular.add(dataApi);
+                                showFilmTV();
+                            }
+                        } catch (JSONException | ParseException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getActivity(), "Tidak ada jaringan internet!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
     private void getTvHorizontal() {
         progressDialog.show();
